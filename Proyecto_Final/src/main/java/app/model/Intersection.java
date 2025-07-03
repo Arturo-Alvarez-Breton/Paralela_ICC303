07e4267486ac194a23d3c34c2b577993688ab334
Arturo <axab0001@ce.pucmm.edu.do>
2025-07-02 20:02:47 -0400

    implementado de PriorityBlockingQueue con comparator

--- Proyecto_Final/src/main/java/app/model/Intersection.java
+++ Proyecto_Final/src/main/java/app/model/Intersection.java
@@ -1,18 +1,28 @@
 package app.model;
 
+import app.enums.VehicleTypeEnum;
+
 import java.util.concurrent.PriorityBlockingQueue;
 
 public class Intersection {
     private String id;
     boolean rightTurnAllowed;
+
+    // Cola concurrente que ordena vehiculos segun prioridad
     private PriorityBlockingQueue<Vehicle> vehicleQueue;
 
     public Intersection(String id, boolean rightTurnAllowed) {
         this.id = id;
         this.rightTurnAllowed = rightTurnAllowed;
+
+        //Inicializar una cola con comparator que prioriza los vehiculos de emergencia
         this.vehicleQueue = new PriorityBlockingQueue<>(10, (v1, v2) -> {
-            // TODO
-            // Implement a comparator based on vehicle priority, e.g., type or direction
+            if (v1.getType() == VehicleTypeEnum.EMERGENCY && v2.getType() != VehicleTypeEnum.EMERGENCY) {
+                return -1; // v1 tiene mayor prioridad
+            } else if (v1.getType() != VehicleTypeEnum.EMERGENCY && v2.getType() == VehicleTypeEnum.EMERGENCY) {
+                return 1; // v2 tiene mayor prioridad
+            }
+
             return 0;
         });
     }
